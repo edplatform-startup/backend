718cbc80d2e651c1a8472985aaee52f4e016a387
Aryaman Rtunjay <aryamanrtunjay@gmail.com>
2025-11-19 21:32:34 -0800

    Increase chatbot max tokens

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -24,7 +24,7 @@ router.post('/', async (req, res) => {
     useWebSearch = false,
     responseFormat = 'text',
     temperature = 0.5,
-    maxTokens = 600,
+    maxTokens = 1800,
     attachments = [],
     reasoning,
     userId,
